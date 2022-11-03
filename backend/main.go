e7043576ab98e0363f19baba0511c28796d954b7
yuyohi <41873114+yuyohi@users.noreply.github.com>
2022-11-03 22:58:51 +0900

    feat: 最小構成を実装

--- backend/main.go
+++ backend/main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func postTaskHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "create task\n")
+}
+
+func main() {
+	http.HandleFunc("/task", postTaskHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
